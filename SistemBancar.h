8065bc2f8740d07f6397cf22b1b5268757940c45
Laur <224015109+ylaur@users.noreply.github.com>
2026-05-22 19:04:06 +0300

    Update SistemBancar.h

--- SistemBancar.h
+++ SistemBancar.h
@@ -26,6 +26,24 @@ public:
     void actualizeazaToateLimiteleCredit(double nouaLimita); // Metoda ce actualizeaza limita de credit
     void ruleazaDetectieFrauda(); // Metoda ce detecteaza eventualele fraude
     void printeazaJurnal() const; // Metoda ce afiseaza jurnalul de sistem al bancii
+    template<typename T, typename U>
+    std::vector<T*> filtreazaConturi(U criteriu); // Metoda ce returneaza conturi care indeplinesc o anumita conditie
+        std::vector<T*> conturiRezultat;
+        for (Cont* cont : conturi) { // Parcurgem conturile
+            T* contAuxiliar = dynamic_cast<T*>(cont);
+            try {
+                if (contAuxiliar != nullptr && criteriu(contAuxiliar)) { // Verificam daca contul indeplineste criteriile de filtrare
+                    conturiRezultat.push_back(contAuxiliar);
+                }
+            }
+            catch (...) { // In cazul in care U nu este o functie sau expresie lambda, prindem eroarea
+                std::cout << "Eroare fatala\n";
+                jurnalSistem.adaugaInregistrare("Eroare fatala in metoda \"filtrareConturi\"");
+                return std::vector<T*>();
+            }
+        }
+        return conturiRezultat;
+    }
 
     Cont* extrageContDupaIndex(unsigned long long index); // Metoda ce extrage un cont dupa indexul sau
 };
